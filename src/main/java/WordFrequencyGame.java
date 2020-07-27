5de48e77a5cdcfee210c007bac7d194434649a67
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-27 16:10:41 +0800

    [WordFrequencyGame][Alisa] refactor: extract getWordFrequency method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 import java.io.CharArrayWriter;
 
 import java.time.LocalDateTime;
@@ -13,30 +9,33 @@ public class WordFrequencyGame {
 
         String[] words = inputWords.split("\\s+");
 
-        Map<String, Integer> wordCount = getWordCount(words);
+        List<Input> wordFrequencylist = getWordFrequency(words);
 
-        List<Input> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-            Input input = new Input(entry.getKey(), entry.getValue());
-            list.add(input);
-        }
-
-        list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-        return formatResult(list);
+        return formatResult(wordFrequencylist);
 
     }
 
-    private String formatResult(List<Input> wordsList){
+    private List<Input> getWordFrequency(String[] words) {
+        Map<String, Integer> wordCount = getWordCount(words);
+
+        return wordCount
+                .entrySet()
+                .stream()
+                .map(word -> new Input(word.getKey(), word.getValue()))
+                .sorted((word1, word2) -> word2.getWordCount()- word1.getWordCount())
+                .collect(Collectors.toList());
+    }
+
+    private String formatResult(List<Input> wordsList) {
         return wordsList.stream().map(word -> word.getValue() + " " + word.getWordCount()).collect(Collectors.joining("\n"));
     }
 
     private Map<String, Integer> getWordCount(String[] words) {
         Map<String, Integer> wordMap = new HashMap<>();
-        for (String word: words) {
-            if (wordMap.containsKey(word)){
-                wordMap.put(word, wordMap.get(word)+1);
-            }else {
+        for (String word : words) {
+            if (wordMap.containsKey(word)) {
+                wordMap.put(word, wordMap.get(word) + 1);
+            } else {
                 wordMap.put(word, 1);
             }
         }
