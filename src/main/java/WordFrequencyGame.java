ee30ebab0ff5806751cb52909ba3726f7fe27b3f
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-27 16:56:41 +0800

    [WordFrequencyGame][Alisa] refactor: use lambda to simplify

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,38 +7,13 @@ import java.util.stream.Collectors;
 public class WordFrequencyGame {
     public String getResult(String inputWords) {
 
-        String[] words = inputWords.split("\\s+");
-
-        List<Input> wordFrequencylist = getWordFrequency(words);
-
-        return formatResult(wordFrequencylist);
-
-    }
-
-    private List<Input> getWordFrequency(String[] words) {
-        Map<String, Integer> wordCount = getWordCount(words);
-
-        return wordCount
+        return Arrays.stream(inputWords.split("\\s+"))
+                .collect(Collectors.toMap(key -> key, value -> 1, Integer::sum))
                 .entrySet()
                 .stream()
-                .map(word -> new Input(word.getKey(), word.getValue()))
-                .sorted((word1, word2) -> word2.getWordCount()- word1.getWordCount())
-                .collect(Collectors.toList());
-    }
+                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+                .map(e -> e.getKey() + " " + e.getValue())
+                .collect(Collectors.joining("\n"));
 
-    private String formatResult(List<Input> wordsList) {
-        return wordsList.stream().map(word -> word.getValue() + " " + word.getWordCount()).collect(Collectors.joining("\n"));
-    }
-
-    private Map<String, Integer> getWordCount(String[] words) {
-        Map<String, Integer> wordMap = new HashMap<>();
-        for (String word : words) {
-            if (wordMap.containsKey(word)) {
-                wordMap.put(word, wordMap.get(word) + 1);
-            } else {
-                wordMap.put(word, 1);
-            }
-        }
-        return wordMap;
     }
 }
