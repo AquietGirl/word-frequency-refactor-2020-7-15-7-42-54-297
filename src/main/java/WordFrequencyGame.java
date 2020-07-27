0b118d4ed0ccb8dc98846ebca9489a7589d641d2
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-27 14:55:44 +0800

    [WordFrequencyGame][Alisa] refactor: rename variable

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,12 +8,14 @@ import java.io.CharArrayWriter;
 import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr) {
+    public String getResult(String inputWords) {
 
-        String[] arr = inputStr.split("\\s+");
+        String[] words = inputWords.split("\\s+");
+
+
 
         List<Input> inputList = new ArrayList<>();
-        for (String s : arr) {
+        for (String s : words) {
             Input input = new Input(s, 1);
             inputList.add(input);
         }
